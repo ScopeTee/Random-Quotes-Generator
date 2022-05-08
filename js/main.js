220af7648aeac90da58b1b8649c6fffc09ecd3cf
Tayo-Subair Fuad <scopeteeofficial@gmail.com>
2022-05-08 23:01:33 +0200

    fixed the API to show in the Dom

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// $('.card').hide()
+
+// $("button").onclick() {
+//     $('.card').show()
+
+// }
+document.querySelector('button').onclick = showQuote
+function showQuote() {
+fetch("https://favqs.com/api/qotd")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        console.log(data.quote.tags)
+        document.getElementById('placeText').innerText = data.quote.body
+        document.getElementById("author").innerText = '~' + data.quote.author
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
